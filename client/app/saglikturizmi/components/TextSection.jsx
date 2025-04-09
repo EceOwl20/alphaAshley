62f1dd0f003d75f8acd924056d22dcfa2ca63fac
Ece Bayramer <ecebayramer20@gmail.com>
2025-04-09 18:51:14 +0300

    2 page added

--- client/app/saglikturizmi/components/TextSection.jsx
+++ client/app/saglikturizmi/components/TextSection.jsx
@@ -1,9 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
-const TextSection = () => {
+const TextSection = ({header,text,link}) => {
   return (
-    <div >
-      
+    <div className='flex w-screen items-center justify-center py-5 lg:py-0 lg:h-[70vh]'>
+      <div className='flex flex-col w-[90%] lg:w-[50%] items-center justify-center text-center gap-[20px] lg:gap-[45px]'>
+        <h4 className='text-[24px] lg:text-[32px] font-semibold text-darkGray leading-[40px]'>{header}</h4>
+        <p className='max-w-[912px] text-[20px] lg:text-[24px] text-lightGray font-normal leading-[24px]'>{text}</p>
+        {link && (
+           <p className='max-w-[912px] text-[20px] lg:text-[24px] text-lightGray font-normal leading-[24px]'> - CV'lerinizi  <Link href="info.alphaashley@gmail.com" className='hover:text-[#ff214f]'>info.alphaashley@gmail.com</Link>mail adresine gönderebilirsiniz.</p>
+        )}
+      </div>
     </div>
   )
 }
--- client/app/saglikturizmi/page.js
+++ client/app/saglikturizmi/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../hakkimizda/components/Banner'
+import TextSection from './components/TextSection'
 
 const page = () => {
   return (
     <div>
       <Banner/>
+      <TextSection header="Sağlık Turizmi" text="Sağlık turizmi, dünya genelinde hızla büyüyen ve giderek daha fazla insanın tercih ettiği bir seyahat ve tedavi biçimi haline gelmektedir. Alpha Ashley Danışmanlık olarak, sağlık turizmi sektöründe yer alan tesisler, hastaneler, klinikler ve sağlık hizmeti sağlayıcıları için stratejik planlama, pazar araştırması ve operasyonel destek gibi alanlarda danışmanlık hizmetleri sunuyoruz. Sunduğumuz çözümler, müşterilerimizin uluslararası pazarda rekabet edebilirliğini artırmak, hedef kitlelerine ulaşmak ve kaliteli sağlık hizmetlerini etkili bir şekilde sunmak için tasarlanmıştır. Sağlık turizmi sektöründeki deneyimimiz ve uzmanlığımızla, müşterilerimizin başarılı olmalarına yardımcı olmak için çalışıyoruz ve bu alanda gelecekteki büyüme ve fırsatları yakalamalarına destek sağlıyoruz." link={false}/>
     </div>
   )
 }
