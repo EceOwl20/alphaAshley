da2509baff0617a72fea837e5104ab8405d0afab
Ece Bayramer <ecebayramer20@gmail.com>
2025-04-10 13:52:32 +0300

    edited

---
+++ client/app/blog/blog-saglik-turizmi/page.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import img2 from "@/public/images/homepage/blog-2.jpg"
+import SubBlogSection from "../../blog/components/SubBlogSection"
+
+const array = [
+    {
+      heading: "1. Yüksek Kaliteli Sağlık Hizmetleri: ",
+      text: "Türkiye, uluslararası standartlara uygun modern tıbbi altyapısıyla donatılmış birçok sağlık kuruluşuna ev sahipliği yapar. Bu kurumlar, deneyimli ve uzman doktorlarıyla yüksek kaliteli sağlık hizmetleri sunarlar."
+    },
+    {
+      heading: "2. Rekabetçi Fiyatlar: ",
+      text: "Türkiye'deki sağlık hizmetleri, Avrupa ve Amerika Birleşik Devletleri gibi diğer ülkelere kıyasla daha uygun fiyatlarla sunulur. Bu durum, Türkiye'yi sağlık turizmi açısından cazip hale getirir."
+    },
+    {
+        heading: "3. Estetik Cerrahi ve Diş Turizmi:  ",
+        text: " Türkiye, özellikle estetik cerrahi ve diş tedavileri gibi alanlarda uluslararası alanda tanınmış bir üne sahiptir. Uygun fiyatlar, yüksek kalite ve deneyimli doktorlar, Türkiye'yi bu alanlarda tercih edilen bir destinasyon haline getirir."
+      },
+      {
+        heading: "4. Kültürel ve Tarihi Zenginlik:",
+        text: "Sağlık turizmiyle birleştirildiğinde, Türkiye'nin zengin kültürel ve tarihi mirasıyla birleşen bir tatil deneyimi sunması da önemli bir avantajdır. Tedavi sürecinin ardından hastalar, tarihi yerleri keşfedebilir, lezzetli yöresel yemeklerin tadını çıkarabilir ve eşsiz doğal güzellikleri keşfedebilirler."
+      },
+      {
+        heading: "5. Kolay Erişim:  ",
+        text: "Türkiye, birçok Avrupa ve Orta Doğu ülkesinden kolayca ulaşılabilir konumdadır. Modern havaalanları, iyi geliştirilmiş ulaşım ağı ve kolay vize politikaları, sağlık turistlerinin Türkiye'yi tercih etmelerini kolaylaştırır."
+      },
+      {
+        heading: "",
+        text: "Türkiye'de sağlık turizmi, her geçen gün daha da popüler hale gelirken, ülkenin sunduğu kaliteli hizmetler, uygun fiyatlar ve benzersiz tatil deneyimi, bu sektördeki büyümeyi desteklemeye devam ediyor. Türkiye'yi sağlık turizmi için bir destinasyon olarak keşfetmek, hem sağlık sorunlarıyla başa çıkmak hem de unutulmaz bir tatil deneyimi yaşamak isteyenler için mükemmel bir seçenektir."
+      },
+     
+    
+  ];
+
+const page = () => {
+    
+  return (
+    <div>
+      <SubBlogSection img={img2} header="2023’ün En Harika Tatil Şehri: Antalya"
+       text="Türkiye, tarih boyunca farklı medeniyetlere ev sahipliği yapmış zengin kültürel mirasıyla tanınırken, son yıllarda sağlık turizmi alanında da dikkatleri üzerine çekiyor. Peki, Türkiye'nin bu alandaki potansiyeli nedir ve sağlık turizminde neden tercih edilmeye başlandı? İşte Türkiye'nin sağlık turizmi açısından sunduğu ilginç özellikler:" 
+       text2="Siz de Türkiye'de sağlık turizmi hakkında daha fazla bilgi edinmek ve sağlık sorunlarınızı çözmek için Türkiye'yi tercih edebilirsiniz. Sağlık turizminde Türkiye'nin sunduğu avantajları keşfedin ve kendinizi evinizde hissedeceğiniz kaliteli hizmetlerle şımartın!" array={array}/>
+    </div>
+  )
+}
+
+export default page
--- client/app/blog/components/BlogGrid.jsx
+++ client/app/blog/components/BlogGrid.jsx
@@ -22,27 +22,27 @@ const BlogGrid = () => {
         <div className='flex flex-col border border-[#CCCCCC] items-center justify-start'>
             <Image src={img2} alt='blog' width={img2.width} height={img2.height} className='w-full object-cover max-h-[300px]'/>
             <div className='flex flex-col w-[90%] items-start justify-start text-start gap-[23px] p-[35px]'>
-                <Link href="/" className='text-[16px] text-[#232323] font-montserrat leading-[23px] hover:text-[#ff214f] mb-[5px]'>Türkiye'de Sağlık Turizmi: Keşfedilmeyi Bekleyen Bir Hazine</Link>
+                <Link href="/blog/blog-saglik-turizmi" className='text-[16px] text-[#232323] font-montserrat leading-[23px] hover:text-[#ff214f] mb-[5px]'>Türkiye'de Sağlık Turizmi: Keşfedilmeyi Bekleyen Bir Hazine</Link>
                 <div className='flex h-[1px] w-full bg-[#CCCCCC]'></div>
-                <p className='text-[14px] font-roboto line-clamp6 leading-[24px] font-normal text-lightGray'>Türkiye, tarih boyunca farklı medeniyetlere ev sahipliği yapmış zengin kültürel mirasıyla tanınırken, son yıllarda sağlık turizmi alanında da dikkatleri üzerine çekiyor. Peki, Türkiye'nin bu alandaki potansiyeli nedir ve sağlık turizminde neden tercih edilmeye başlandı?...<Link href="/" className="font-montserrat text-[14px] font-roboto leading-[24px] font-normal text-[#939393] hover:text-[#FF214F] cursor-pointer">Devamını Oku</Link></p>
+                <p className='text-[14px] font-roboto line-clamp6 leading-[24px] font-normal text-lightGray'>Türkiye, tarih boyunca farklı medeniyetlere ev sahipliği yapmış zengin kültürel mirasıyla tanınırken, son yıllarda sağlık turizmi alanında da dikkatleri üzerine çekiyor. Peki, Türkiye'nin bu alandaki potansiyeli nedir ve sağlık turizminde neden tercih edilmeye başlandı?...<Link href="/blog/blog-saglik-turizmi" className="font-montserrat text-[14px] font-roboto leading-[24px] font-normal text-[#939393] hover:text-[#FF214F] cursor-pointer">Devamını Oku</Link></p>
             </div>
         </div>
 
         <div className='flex flex-col border border-[#CCCCCC] items-center justify-start'>
             <Image src={img3} alt='blog' width={img3.width} height={img3.height} className='w-full object-cover max-h-[300px]'/>
             <div className='flex flex-col w-[90%] items-start justify-start text-start gap-[23px] p-[35px]'>
-                <Link href="/" className='text-[16px] text-[#232323] font-montserrat leading-[23px] hover:text-[#ff214f] mb-[5px]'>Antalya'yı Keşfetmek: Sağlık Destinasyonua</Link>
+                <Link href="/blog/blog-saglik-destinasyonu" className='text-[16px] text-[#232323] font-montserrat leading-[23px] hover:text-[#ff214f] mb-[5px]'>Antalya'yı Keşfetmek: Sağlık Destinasyonua</Link>
                 <div className='flex h-[1px] w-full bg-[#CCCCCC]'></div>
-                <p className='text-[14px] font-roboto line-clamp6 leading-[24px] font-normal text-lightGray'>Akdeniz'in muhteşem sahil şeridi boyunca yer alan Antalya, sadece nefes kesen doğal güzelliği ile değil, aynı zamanda yenilenme ve rahatlama arayan gezginler için çeşitli sağlık ve wellness deneyimleri sunmasıyla da dikkat çekiyor. İşte Antalya'nın neden sağlık...<Link href="/" className="font-montserrat text-[14px] font-roboto leading-[24px] font-normal text-[#939393] hover:text-[#FF214F] cursor-pointer">Devamını Oku</Link></p>
+                <p className='text-[14px] font-roboto line-clamp6 leading-[24px] font-normal text-lightGray'>Akdeniz'in muhteşem sahil şeridi boyunca yer alan Antalya, sadece nefes kesen doğal güzelliği ile değil, aynı zamanda yenilenme ve rahatlama arayan gezginler için çeşitli sağlık ve wellness deneyimleri sunmasıyla da dikkat çekiyor. İşte Antalya'nın neden sağlık...<Link href="/blog/blog-saglik-destinasyonu" className="font-montserrat text-[14px] font-roboto leading-[24px] font-normal text-[#939393] hover:text-[#FF214F] cursor-pointer">Devamını Oku</Link></p>
             </div>
         </div>
 
         <div className='flex flex-col border border-[#CCCCCC] items-center justify-start'>
             <Image src={img4} alt='blog' width={img4.width} height={img4.height} className='w-full object-cover max-h-[300px]'/>
             <div className='flex flex-col w-[90%] items-start justify-start text-start gap-[23px] p-[35px]'>
-                <Link href="/" className='text-[16px] text-[#232323] font-montserrat leading-[23px] hover:text-[#ff214f] mb-[5px]'>Türkiye'de Sağlık Turizmi: İyileşme ve Keşif İçin Mükemmel Bir Durak</Link>
+                <Link href="/blog/blog-iyilesme-ve-kesif" className='text-[16px] text-[#232323] font-montserrat leading-[23px] hover:text-[#ff214f] mb-[5px]'>Türkiye'de Sağlık Turizmi: İyileşme ve Keşif İçin Mükemmel Bir Durak</Link>
                 <div className='flex h-[1px] w-full bg-[#CCCCCC]'></div>
-                <p className='text-[14px] font-roboto line-clamp6 leading-[24px] font-normal text-lightGray'>Türkiye, son yıllarda sağlık turizminde giderek artan bir popülerlik kazanmaktadır. Doğal güzellikleri, tarihi zenginlikleri ve yüksek kaliteli sağlık hizmetleri ile Türkiye, dünya genelinde sağlık turistlerinin dikkatini çekmektedir. Peki, Türkiye'de sağlık turizmi ne sunuyor ve bu alandaki devlet teşvikleri nelerdir? İşte bu konuda daha fazla bilgi:...<Link href="/" className="font-montserrat text-[14px] font-roboto leading-[24px] font-normal text-[#939393] hover:text-[#FF214F] cursor-pointer">Devamını Oku</Link></p>
+                <p className='text-[14px] font-roboto line-clamp6 leading-[24px] font-normal text-lightGray'>Türkiye, son yıllarda sağlık turizminde giderek artan bir popülerlik kazanmaktadır. Doğal güzellikleri, tarihi zenginlikleri ve yüksek kaliteli sağlık hizmetleri ile Türkiye, dünya genelinde sağlık turistlerinin dikkatini çekmektedir. Peki, Türkiye'de sağlık turizmi ne sunuyor ve bu alandaki devlet teşvikleri nelerdir? İşte bu konuda daha fazla bilgi:...<Link href="/blog/blog-iyilesme-ve-kesif" className="font-montserrat text-[14px] font-roboto leading-[24px] font-normal text-[#939393] hover:text-[#FF214F] cursor-pointer">Devamını Oku</Link></p>
             </div>
         </div>
       </div>
