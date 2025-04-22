e275c098291334bbe1e20ee9abefbcbfb3dc7760
Ece Bayramer <ecebayramer20@gmail.com>
2025-04-22 11:33:16 +0300

    edited

--- client/app/Homepage/Location.jsx
+++ client/app/Homepage/Location.jsx
@@ -4,16 +4,19 @@ const Location = () => {
   return (
     <div>
       <iframe
-      src=' https://www.google.com/maps/d/embed?mid=1dK2Y5uAD3PQ5X0a6ywVKUPeG8IIx0UQ&usp=sharing'
+      src=' https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6182075.471293318!2d22.71608915553109!3d40.836934707679816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x76a56f36e0259d7%3A0x6d6b9e4944c7d48a!2zQWxwaGEgQXNobGV5IEXEn2l0aW0gdmUgRGFuxLHFn21hbmzEsWs!5e0!3m2!1str!2str!4v1745310465236!5m2!1str!2str'
        width="100%"
        height="500px"
        style={{ border: 0 }}
        allowFullScreen
        loading="lazy"
        referrerPolicy="no-referrer-when-downgrade"
+      
        >
         
       </iframe>
+
+     
     </div>
   )
 }
