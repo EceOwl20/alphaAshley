13b176da6f4697495163274e1db1fda1f11e76ab
Ece Bayramer <ecebayramer20@gmail.com>
2025-07-17 15:05:51 +0300

    image 21 added

--- client/app/Homepage/PersonCarousel.jsx
+++ client/app/Homepage/PersonCarousel.jsx
@@ -24,8 +24,9 @@ import img17 from "@/public/images/homepage/person17.png"
 import img18 from "@/public/images/homepage/person18.png"
 import img19 from "@/public/images/homepage/person19.png"
 import img20 from "@/public/images/homepage/person20.jpeg"
+import img21 from "@/public/images/homepage/person21.jpeg"
 
-const images =[img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14,img15,img16, img17, img18, img19, img20]
+const images =[img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14,img15,img16, img17, img18, img19, img20, img21]
 
 const PersonCarousel = () => {
     const [emblaRef, emblaApi] = useEmblaCarousel({  loop: true,
